ba56cf9e5fc137304b61ec5a1f85564d03af95f7
Yeonggyu Yun <55074079+yoonyg5512@users.noreply.github.com>
2023-02-02 13:49:01 -0600

    Add files via upload

--- HW2/Simulation/Search.jl
+++ HW2/Simulation/Search.jl
@@ -10,14 +10,14 @@ using Parameters, Statistics, Random, Distributions, Interpolations, Optim, Plot
 
 ##### 1. Housekeeping
 
+Random.seed!(1234)
+
 @with_kw struct Params
     T::Int64 = 360 # Corresponds to 30 years
     r::Float64 = 0.04 # Interest rate
     β::Float64 = (1/(1+r))^(1/12) # Time discount
     δ::Float64 = 0.033 # Layoff probability
     b::Float64 = 0.1 # Unemployment insurance
-    ψ_u::Float64 = 0.4 # Decrease probability of human capital
-    ψ_e::Float64 = 0.6 # Increase probability of human capital
 
     # Grids for human capital h and search effort s
 
@@ -32,9 +32,16 @@ using Parameters, Statistics, Random, Distributions, Interpolations, Optim, Plot
     μ_w::Float64 = 0.5 # Mean of wage offer
     m::Float64 = 3.0 # Tauchen maximum value
     N_w::Int64 = 41 # Grid size for wage offers
+
+    # Simulation
+
+    N_i::Int64 = 1000 # Number of individuals to simulate
 end
 
 mutable struct Results
+    ψ_u::Float64
+    ψ_e::Float64
+
     U::Array{Float64, 2}
     S::Array{Float64, 2}
     W::Array{Float64, 3}
@@ -46,6 +53,9 @@ end
 function Initialize()
     pars = Params()
 
+    ψ_u::Float64 = 0.4 # Decrease probability of human capital
+    ψ_e::Float64 = 0.6 # Increase probability of human capital
+
     U = zeros(pars.N_h, pars.T)
     S = zeros(pars.N_h, pars.T)
     W = zeros(pars.N_h, pars.T, pars.N_w)
@@ -53,7 +63,7 @@ function Initialize()
     w_grid = zeros(pars.N_w)
     Π_w = zeros(pars.N_w)
 
-    res = Results(U, S, W, w_grid, Π_w)
+    res = Results(ψ_u, ψ_e, U, S, W, w_grid, Π_w)
     return pars, res
 end
 
@@ -102,8 +112,8 @@ end
 ##### 2. Value function iteration
 
 function Bellman(pars, res)
-    @unpack b, β, s_grid, h_grid, T, N_w, N_h, N_s, ψ_e, ψ_u, δ = pars
-    @unpack Π_w, w_grid, U, W = res
+    @unpack b, β, s_grid, h_grid, T, N_w, N_h, N_s, δ = pars
+    @unpack Π_w, w_grid, U, ψ_e, ψ_u, W = res
 
     U_cand = zeros(N_h, T)
     W_cand = zeros(N_h, T, N_w)
@@ -169,3 +179,68 @@ end
 
 pars, res = Initialize()
 VFI(pars, res)
+
+##### 4. Simulation
+
+mutable struct dat_sim
+    H_path::Array(Float64, 2) # Human capital h
+    W_path::Array(Float64, 2) # Wages w
+    U_path::Array(Float64, 2) # 1 if employed, 0 if unemployed
+end
+
+function Init_sim(pars)
+    H_path::Array(Float64, 2) = zeros(pars.N_i, pars.T)
+    W_path::Array(Float64, 2) = zeros(pars.N_i, pars.T)
+    U_path::Array(Float64, 2) = zeros(pars.N_i, pars.T)
+
+    simulated = dat_sim(H_path, W_path, U_path)
+    return simulated
+end
+
+function simulate(pars, res)
+    @unpack b, β, s_grid, h_grid, T, N_w, N_h, N_s, δ, N_i = pars
+    @unpack Π_w, w_grid, U, S, W, ψ_e, ψ_u= res
+
+    μ_h = 2.5 # Mean of Uniform(2,3)
+    σ_h = sqrt(1/12) # Standard deviation of Uniform(2,3)
+
+    H_path[:,1] = rand(Normal(μ_h, σ_h), N_i)
+    U_path[:,1] .= 0
+    W_path[:,1] .= b
+    for i in 1:N_i
+        H_path[i,1] = findmax(-abs.(H_path[i,1] .- h_grid))[2] # map initial human capital onto the discretized set of hs
+    end
+    
+    for i in 1:N_i
+        for t in 2:T
+            probs = rand(Uniform(0,1), 2) # probs[1]: unemployment prob, probs[2]: human capital change prob
+
+            if U_path[i,t-1] == 0
+                
+            elseif U_path[i,t-1] == 1
+                if probs[1] < δ
+                    U_path[i,t] = 0
+                    W_path[i,t] = b
+                    
+                    if probs[2] < ψ_e
+                        H_path[i,t] = minimum(H_path[i,t-1]+1, N_h)
+                    elseif probs[2] >= ψ_e
+                        H_path[i,t] = H_path[i,t-1]
+                    end
+                elseif probs[2] >= δ
+                    U_path[i,t] = 1
+                    W_path[i,t] = W_path[i,t-1]
+
+                    if probs[2] < ψ_e
+                        H_path[i,t] = minimum(H_path[i,t-1]+1, N_h)
+                    elseif probs[2] >= ψ_e
+                        H_path[i,t] = H_path[i,t-1]
+                    end
+                end
+            end
+        end
+    
+    end
+    
+end
+
