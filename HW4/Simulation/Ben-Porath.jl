8f40250fd97642c16cd18c925b4d1e2f5a74f726
Yeonggyu Yun <55074079+yoonyg5512@users.noreply.github.com>
2023-02-18 23:55:01 -0600

    Add files via upload

--- HW4/Simulation/Ben-Porath.jl
+++ HW4/Simulation/Ben-Porath.jl
@@ -27,14 +27,16 @@ Random.seed!(1234)
 
     N_h::Int64 = 100
     N_k::Int64 = 20
-    N_s::Int64 = 20
+    N_s::Int64 = 40
     h_grid::Array{Float64, 1} = range(start = 0.0, stop = 10.0, length = N_h)
     k_grid::Array{Float64, 1} = range(start = 0.0, stop = 20.0, length = N_k)
     s_grid::Array{Float64, 1} = range(start = 0.0, stop = 1.0, length = N_s)
 
     # Simulation
 
-    N_i::Int64 = 1000 # Number of individuals to simulate
+    N_i::Int64 = 10000 # Number of individuals to simulate
+    T_sim::Int64 = 30 # Number of years to simulate
+    burnin::Int64 = 45
 end
 
 mutable struct Results
@@ -46,6 +48,11 @@ mutable struct Results
 
     S::Array{Float64, 3} # Investment in human capital
     K::Array{Float64, 3} # Asset
+
+    # Discretized grid of z
+
+    zs::Array{Float64, 1}
+    Π_z::Array{Float64, 1}
 end
 
 function Initialize()
@@ -54,8 +61,10 @@ function Initialize()
     V::Array{Float64, 3} = zeros(pars.N_k, pars.T, pars.N_h)
     S::Array{Float64, 3} = zeros(pars.N_k, pars.T, pars.N_h)
     K::Array{Float64, 3} = zeros(pars.N_k, pars.T, pars.N_h)
+    zs::Array{Float64, 1} = zeros(20)
+    Π_z::Array{Float64, 1} = zeros(20)
 
-    res = Results(V, S, K)
+    res = Results(V, S, K, zs, Π_z)
     return pars, res
 end
 
@@ -86,8 +95,8 @@ function Bellman(pars, res)
     ## Discretize the space of z following Tauchen
     
     N_z = 20
-    zN = 3*σ_z
-    z1 = -zN
+    zN = μ_z + 3*σ_z
+    z1 = μ_z - 3*σ_z
 
     Π_z = zeros(N_z)
     zs = range(start = z1, stop = zN, length = N_z)
@@ -121,10 +130,13 @@ function Bellman(pars, res)
     s_cand[:,T,:] .= 0.0;
     
     for t in collect((T-1):-1:1)
+        println(t)
         v_interp = interpolate(v_cand[:,t+1,:], BSpline(Linear()));
             for (i_k, k) in enumerate(k_grid)
-                for (i_h, h) in enumerate(h_grid)
 
+            println(i_k)
+                for (i_h, h) in enumerate(h_grid)
+                        println(i_h)
                         function v_tomorrow(i_kp, i_sp)
                             v_t = 0.0;
                             H = h + (h * s_interp(i_sp))^α
@@ -145,7 +157,7 @@ function Bellman(pars, res)
                         lower = [1.0, 1.0]
                         upper = [Float64(get_index((1+r)*k + R^(t-1) * h, k_grid)), Float64(N_k)]
                         
-                        if lower[1] == upper[2]
+                        if lower[1] == upper[1]
                             obj_t(x) = - v_today(lower[1], x)
                             opt = optimize(obj_t, lower[2], upper[2])
 
@@ -157,7 +169,7 @@ function Bellman(pars, res)
                             s_cand[i_k, t, i_h] = s_today
                             v_cand[i_k, t, i_h] = v_now
                         else
-                            opt = optimize(obj, lower, upper, [(lower[1] +upper[1]) / 2, (lower[2] + upper[2])/ 2])
+                            opt = optimize(obj, lower, upper, [(lower[1] +upper[1]) / 2, (lower[2] + upper[2])/ 2], NelderMead())
 
                             k_tomorrow = k_interp(opt.minimizer[1])
                             s_today = s_interp(opt.minimizer[2])
@@ -170,27 +182,90 @@ function Bellman(pars, res)
                     end
                 end
             end
-        println(t)
     end
-    return k_cand, s_cand, v_cand
+    return k_cand, s_cand, v_cand, zs, Π_z
 end
 
 function Solve_Model(pars, res)
-    res.K, res.S, res.V = Bellman(pars, res)
+    res.K, res.S, res.V, res.zs, res.Π_z = Bellman(pars, res)
 end
 
 ## 3. Simulation
 
 mutable struct Sims
-    E::Array{Float64, 2} # Earnings of each individual of ages
+    E::Array{Float64, 2} # Earnings of each individual at each year
+    A::Array{Int64, 2} # Ages of each individual at each year
 end
 
 function Init_sim(pars)
-    E::Array{Float64, 2} = zeros(pars.N_i, 30)
-    A::Array{Float64, 2} = zeros(pars.N_i, 30)
+    E::Array{Float64, 2} = zeros(pars.N_i, pars.T_sim) # 30 years of panel
+    A::Array{Int64, 2} = zeros(pars.N_i, pars.T_sim)
+
+    sims = Sims(E, A)
+    return sims
 end
 
 function Simulate(pars, res)
-    @unpack N_i = pars
-    @unpack K, S, V = res
+    @unpack N_i, T_sim, burnin, μ_h, σ_h, R, N_k, T, α = pars
+    @unpack K, S, V, zs, Π_z = res
+
+    E_sim = zeros(N_i, T_sim+burnin)
+    A_sim = zeros(N_i, T_sim+burnin)
+
+    A_start = rand(1:T, N_i)
+    A_sim[:,1] = A_start
+    
+    h_now = rand(Normal(μ_h, σ_h), N_i)
+    h_now[h_now .< 0] .= 0.0
+    h_now = get_index.(h_now, h_grid)
+    k_start = rand(1:N_k, N_i)
+    kp = zeros(N_i)
+    sp = zeros(N_i) 
+
+    h_interp = interpolate(h_grid, BSpline(Linear()))
+    K_interp = interpolate(K, BSpline(Linear()))
+    S_interp = interpolate(S, BSpline(Linear()))
+
+    for i in 1:N_i
+        E_sim[i,1] = R^(A_sim[i,1] - 1) * h_interp(h_now[i]) * S_interp(k_start, A_sim[i,1], h_now[i])
+        kp[i] = get_index(K_interp(k_start, A_sim[i,1], h_now[i]), k_grid)
+        sp[i] = get_index(S_interp(k_start, A_sim[i,1], h_now[i]), s_grid)
+    end
+
+    for i in 1:N_i
+        for j in 2:T_sim
+            if A_sim[i,j-1] == T
+                A_sim[i,j] = 1
+
+                h_init = maximum(rand(Normal(μ_h, σ_h)), 0.0)
+                h_now[i] = get_index(h_init, h_grid)
+                E_sim[i,j] = h_interp(h_now[i]) * (1 - S_interp(1, 1, h_now[i]))
+
+                kp[i] = get_index(K_interp(1, 1, h_now[i]), k_grid)
+                sp[i] = get_index(S_interp(1, 1, h_now[i]), s_grid)
+
+            elseif A_sim[i,j-1] < T
+                draw_z = rand(Uniform(0,1))
+                z_now = zs[Int(ceil(get_index(draw_z, cumsum(Π_z))))]
+                h_now[i] = get_index(exp(z_now) * (h_interp(h_now[i]) + (h_interp(h_now[i]) * sp[i])^α), h_grid)
+                
+                A_sim[i,j] = A_sim[i,j-1] + 1
+                E_sim[i,j] = R^(A_sim[i,j] - 1) * h_interp(h_now[i]) * (1 - S_interp(kp[i], A_sim[i,j], h_now[i]))
+
+                kp[i] = get_index(K_interp(kp[i], A_sim[i,j], h_now[i]), k_grid)
+                sp[i] = get_index(S_interp(kp[i], A_sim[i,j], h_now[i]), s_grid)
+            end
+        end
+    end
+
+    return E_sim[:, (burnin+1):(T_sim+burnin)], A_sim[:, (burnin+1):(T_sim+burnin)]
 end
+
+function Simulate(pars, res, sims)
+    sims.E, sims.A = Simulate(pars, res)
+end
+
+## 4. Write into panel data
+
+pars, res = Initialize()
+Solve_Model(pars, res)
