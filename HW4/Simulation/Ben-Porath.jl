c534dbb08b8669fc6e819e60b6dba01e824fe103
Yeonggyu Yun <55074079+yoonyg5512@users.noreply.github.com>
2023-02-20 11:47:07 -0600

    Add files via upload

--- HW4/Simulation/Ben-Porath.jl
+++ HW4/Simulation/Ben-Porath.jl
@@ -25,7 +25,7 @@ Random.seed!(1234)
 
     # Grids for human capital h and saving choice b and piece-rate ω
 
-    N_h::Int64 = 20
+    N_h::Int64 = 30
     N_k::Int64 = 20
     N_s::Int64 = 40
     h_grid::Array{Float64, 1} = range(start = 0.0, stop = 10.0, length = N_h)
@@ -134,40 +134,41 @@ function Bellman(pars, res)
     for t in collect((T-1):-1:1)
         v_interp = interpolate(v_cand[:,t+1,:], BSpline(Linear()));
 
-        function v_tomorrow(i_kp, i_sp, h)
-            v_t = 0.0;
-            H = h + (h * s_interp(i_sp))^α
-            
-            for (n_z, z)  in enumerate(zs) 
-                h_next = exp(z) * H
-                i_h_next = get_index(h_next, h_grid)
-
-                v_t += Π_z[n_z] * v_interp(i_kp, i_h_next)
-            end
-
-            return v_t
-        end
-    
-        function v_today(i_sp, h, k)
-            budget = (1+r)*k + R^(t-1) * h * (1-s_interp(i_sp))
-            v_given_s(i_kp) = ((budget - k_interp(i_kp))^(1-σ) - 1) / (1 - σ) + β * v_tomorrow(i_kp, i_sp, h)
-            lower_k = 1.0 
-            upper_k = get_index(budget, k_grid)
-            obj_k(i_kp) = - v_given_s(i_kp)
-            opt_k = optimize(obj_k, lower_k, upper_k)
-
-            return opt_k.minimizer[1], -opt_k.minimum
-        end
-
         for (i_k, k) in enumerate(k_grid)
             for (i_h, h) in enumerate(h_grid)
-                obj_s(i_sp) = - v_today(i_sp, h, k)[2]
+
+                function v_tomorrow(i_kp, i_sp)
+                    v_t = 0.0;
+                    H = h + (h * s_interp(i_sp))^α
+                    
+                    for (n_z, z)  in enumerate(zs) 
+                        h_next = exp(z) * H
+                        i_h_next = get_index(h_next, h_grid)
+        
+                        v_t += Π_z[n_z] * v_interp(i_kp, i_h_next)
+                    end
+        
+                    return v_t
+                end
+            
+                function v_today(i_sp)
+                    budget = (1+r)*k + R^(t-1) * h * (1-s_interp(i_sp))
+                    v_given_s(i_kp) = ((budget - k_interp(i_kp))^(1-σ) - 1) / (1 - σ) + β * v_tomorrow(i_kp, i_sp)
+                    lower_k = 1.0 
+                    upper_k = get_index(budget, k_grid)
+                    obj_k(i_kp) = - v_given_s(i_kp)
+                    opt_k = optimize(obj_k, lower_k, upper_k)
+        
+                    return opt_k.minimizer[1], -opt_k.minimum
+                end
+
+                obj_s(i_sp) = - v_today(i_sp)[2]
                 lower_s = 1.0
                 upper_s = Float64(N_k)
 
                 opt_s = optimize(obj_s, lower_s, upper_s)
                 s_today = s_interp(opt_s.minimizer[1])
-                k_tomorrow = k_interp(v_today(opt_s.minimizer[1], h, k)[1])
+                k_tomorrow = k_interp(v_today(opt_s.minimizer[1])[1])
                 v_now  = - opt_s.minimum
 
                 k_cand[i_k, t, i_h] = k_tomorrow
@@ -189,6 +190,8 @@ end
 mutable struct Sims
     E::Array{Float64, 2} # Earnings of each individual at each year
     A::Array{Int64, 2} # Ages of each individual at each year
+    H::Array{Float64, 2}
+    S::Array{Float64, 2}
     id::Array{Int64, 2} # Individual id
 end
 
@@ -196,39 +199,51 @@ function Init_sim(pars)
     E::Array{Float64, 2} = zeros(pars.N_i, pars.T_sim) # 30 years of panel
     A::Array{Int64, 2} = zeros(pars.N_i, pars.T_sim)
     id::Array{Int64, 2} = zeros(pars.N_i, pars.T_sim)
+    H::Array{Float64, 2} = zeros(pars.N_i, pars.T_sim)
+    S::Array{Float64, 2} = zeros(pars.N_i, pars.T_sim)
 
-    sims = Sims(E, A, id)
+    sims = Sims(E, A, H, S, id)
     return sims
 end
 
 function Simulate(pars, res)
-    @unpack N_i, T_sim, burnin, μ_h, σ_h, R, N_k, T, α = pars
+    @unpack N_i, T_sim, burnin, μ_h, σ_h, R, N_k, T, α, h_grid, k_grid, s_grid = pars
     @unpack K, S, V, zs, Π_z = res
+
+    burnin = 150
 
     E_sim = zeros(N_i, T_sim+burnin)
     A_sim = zeros(N_i, T_sim+burnin)
     id_sim = zeros(N_i, T_sim+burnin)
+    H_sim = zeros(N_i, T_sim+burnin)
+    S_sim = zeros(N_i, T_sim+burnin)
 
-    A_start = rand(1:T, N_i)
-    A_sim[:,1] = A_start
-    
-    h_now = rand(Normal(μ_h, σ_h), N_i)
-    h_now[h_now .< 0] .= 0.0
-    for i in 1:N_i
-        h_now[i] = get_index(h_now[i], h_grid)
-    end
-    k_start = rand(1:N_k, N_i)
-    kp = zeros(N_i)
-    sp = zeros(N_i) 
+    KP_sim = zeros(N_i, T_sim+burnin)
 
     h_interp = interpolate(h_grid, BSpline(Linear()))
     K_interp = interpolate(K, BSpline(Linear()))
     S_interp = interpolate(S, BSpline(Linear()))
+    s_interp = interpolate(s_grid, BSpline(Linear()))
+
+    A_start = rand(1:T, N_i)
+    A_sim[:,1] = A_start
+    
+    h_start = rand(Normal(μ_h, σ_h), N_i)
+    h_start[h_start .< 0] .= 0.0
+    H_sim[:,1] = h_start
+    k_start = rand(1:N_k, N_i)
 
     for i in 1:N_i
-        E_sim[i,1] = R^(A_sim[i,1] - 1) * h_interp(h_now[i]) * S_interp(k_start[i], A_sim[i,1], h_now[i])
-        kp[i] = get_index(K_interp(k_start[i], A_sim[i,1], h_now[i]), k_grid)
-        sp[i] = get_index(S_interp(k_start[i], A_sim[i,1], h_now[i]), s_grid)
+        h_start[i] = get_index(h_start[i], h_grid)
+
+        if A_start[i] == 1
+            k_start[i] = 1
+        end
+        KP_sim[i,1] = k_start[i]
+
+        E_sim[i,1] = R^(A_sim[i,1] - 1) * h_interp(h_start[i]) * (1 - S_interp(KP_sim[i,1], A_sim[i,1], h_start[i]))
+        KP_sim[i,2] = get_index(K_interp(KP_sim[i,1], A_sim[i,1], h_start[i]), k_grid)
+        S_sim[i,1] = S_interp(KP_sim[i,1], A_sim[i,1], h_start[i])
     end
 
     id_sim[1,1] = 1
@@ -243,33 +258,41 @@ function Simulate(pars, res)
                 A_sim[i,j] = 1
 
                 h_init = maximum([rand(Normal(μ_h, σ_h)), 0.0])
-                h_now[i] = get_index(h_init, h_grid)
-                E_sim[i,j] = h_interp(h_now[i]) * (1 - S_interp(1, 1, h_now[i]))
+                h_now = get_index(h_init, h_grid)
+                E_sim[i,j] = h_interp(h_now) * (1 - S_interp(1, 1, h_now))
 
-                kp[i] = get_index(K_interp(1, 1, h_now[i]), k_grid)
-                sp[i] = get_index(S_interp(1, 1, h_now[i]), s_grid)
-
+                if j < (T_sim + burnin)
+                    KP_sim[i, j+1] = get_index(K_interp(1, 1, h_now), k_grid)
+                end
+                
+                H_sim[i,j] = h_interp(h_now)
+                S_sim[i,j] = S_interp(1, 1, h_now)
             elseif A_sim[i,j-1] < T
                 id_sim[i,j] = id_sim[i,j-1]
 
                 draw_z = rand(Uniform(0,1))
                 z_now = zs[Int(ceil(get_index(draw_z, cumsum(Π_z))))]
-                h_now[i] = get_index(exp(z_now) * (h_interp(h_now[i]) + (h_interp(h_now[i]) * sp[i])^α), h_grid)
-                
-                A_sim[i,j] = A_sim[i,j-1] + 1
-                E_sim[i,j] = R^(A_sim[i,j] - 1) * h_interp(h_now[i]) * (1 - S_interp(kp[i], A_sim[i,j], h_now[i]))
+                h_now = get_index(exp(z_now) * (H_sim[i,j-1] + (H_sim[i,j-1] * S_sim[i,j-1])^α), h_grid)
 
-                kp[i] = get_index(K_interp(kp[i], A_sim[i,j], h_now[i]), k_grid)
+                A_sim[i,j] = A_sim[i,j-1] + 1
+                E_sim[i,j] = R^(A_sim[i,j] - 1) * h_interp(h_now) * (1 - S_interp(KP_sim[i,j], A_sim[i,j], h_now))
+                
+                if j < (T_sim + burnin)
+                    KP_sim[i,j+1] = get_index(K_interp(KP_sim[i,j], A_sim[i,j], h_now), k_grid)
+                end
                 sp[i] = get_index(S_interp(kp[i], A_sim[i,j], h_now[i]), s_grid)
+
+                H_sim[i,j] = h_interp(h_now)
+                S_sim[i,j] = S_interp(KP_sim[i,j], A_sim[i,j], h_now)
             end
         end
     end
 
-    return E_sim[:, (burnin+1):(T_sim+burnin)], A_sim[:, (burnin+1):(T_sim+burnin)], id_sim[:, (burnin+1):(T_sim+burnin)]
+    return E_sim[:, (burnin+1):(T_sim+burnin)], A_sim[:, (burnin+1):(T_sim+burnin)], id_sim[:, (burnin+1):(T_sim+burnin)], H_sim[:, (burnin+1):(T_sim+burnin)], S_sim[:, (burnin+1):(T_sim+burnin)]
 end
 
 function Simulate_data(pars, res, sims)
-    sims.E, sims.A, sims.id = Simulate(pars, res)
+    sims.E, sims.A, sims.id, sims.H, sims.S = Simulate(pars, res)
 end
 
 ## 4. Write into panel data
@@ -281,13 +304,24 @@ sims = Init_sim(pars)
 Simulate_data(pars, res, sims)
 
 years = repeat(1:pars.T_sim, pars.N_i)
-panel = zeros(pars.T_sim*pars.N_i, 4)
+panel = zeros(pars.T_sim*pars.N_i, 6)
 
 panel[:,1] = vec(sims.id') # ID
 panel[:,2] = years # Year
 panel[:,3] = vec(sims.A') # Age
 panel[:,4] = vec(sims.E') # Earnings
+panel[:,5] = vec(sims.S') # Earnings
+panel[:,6] = vec(sims.H') # Earnings
+
 
 panel = DataFrame(panel, :auto)
-rename!(panel, Symbol.(["ID", "Year", "Age", "Earnings"]))
+rename!(panel, Symbol.(["ID", "Year", "Age", "Earnings", "S", "H"]))
 CSV.write("/Users/Yeonggyu/Desktop/윤영규/대학원 (UW-Madison)/Coursework/Spring 2023/Econ 810 - Advanced Macroeconomics/Week 4/HW/Simulated panel.csv", panel, writeheader = true)
+
+mean_by_age = zeros(pars.T)
+
+for i in 1:pars.T
+    mean_by_age[i] = mean(sims.S[sims.A .== i])
+end
+
+plot(mean_by_age)
